f6958dee2495a8c3013a80245d203931edccca35
Daewoong Jeon <daewoong.jeon@kt.com>
2024-11-19 23:07:32 +0900

    commit

---
+++ view/src/main/java/untitled/domain/AvailableStatusUpdated.java
@@ -0,0 +1,17 @@
+package untitled.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.Data;
+import untitled.infra.AbstractEvent;
+
+@Data
+public class AvailableStatusUpdated extends AbstractEvent {
+
+    private String memberId;
+    private Integer cost;
+    private String id;
+    private String status;
+    private Date rentalDate;
+    private Date requiredReturnDate;
+}
---
+++ view/src/main/java/untitled/domain/RentalStatusUpdated.java
@@ -0,0 +1,17 @@
+package untitled.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.Data;
+import untitled.infra.AbstractEvent;
+
+@Data
+public class RentalStatusUpdated extends AbstractEvent {
+
+    private String memberId;
+    private Integer cost;
+    private String id;
+    private String status;
+    private Date rentalDate;
+    private Date requiredReturnDate;
+}
